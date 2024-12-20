c449e53d2a6cd9aa4909b95ac322f00120cd81f2
liamjohnharvey <liamjohnharvey@gmail.com>
2024-12-20 21:40:55 +1000

    Make large asteroids split when shot

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from constants import ASTEROID_MIN_RADIUS
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
@@ -7,6 +9,20 @@ class Asteroid(CircleShape):
     
     def split(self):
         self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 50)
+
+        vector_1 = self.velocity.rotate(random_angle)
+        vector_2 = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x,self. position.y, new_radius)
+        asteroid.velocity = vector_1 * 1.2
+        asteroid = Asteroid(self.position.x,self. position.y, new_radius)
+        asteroid.velocity = vector_2 * 1.2
     
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
